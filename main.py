8f575962479f50cdc214394bf2d98a11a0a16306
Léo Leclerc <leoleclerc905@gmail.com>
2022-04-06 01:00:47 +0200

    V0.2 affichage du score dans un boxlayout a part

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import kivy
 import random
 from kivy.app import App
 from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
 
-class MainWidget(FloatLayout):
+class MainWidget(BoxLayout):
     pos_x = NumericProperty()
     pos_y = NumericProperty()
+    score = NumericProperty()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.pos_x = random.randint(0, 100) / 100
         self.pos_y = random.randint(0, 100) / 100
+        self.score = 0
 
     def on_button_press(self):
         print("pouet")
+        self.score += 1
         self.pos_x = random.randint(0, 100) / 100
         self.pos_y = random.randint(0, 100) / 100
 
@@ -30,6 +35,8 @@ class ClickMeButton(Button):
     #     MainWidget.pos_x = random.randint(0, 1265)
     #     MainWidget.pos_y = random.randint(0, 605)
 
+class ScoreLabel(Label):
+    pass
 
 class ClickFaster(App):
     def build(self):
