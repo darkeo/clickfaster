20d0200b00d2469d040b9d9a2beae0812b66e4b8
Léo Leclerc <leoleclerc905@gmail.com>
2022-04-13 04:47:16 +0200

    V1, timer de + en + rapide + fix fenetre

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
+# temps de base, arbitraire pour l'instant
+base_time = 5
 
 class MainWidget(BoxLayout):
     pos_x = NumericProperty()
@@ -29,23 +31,28 @@ class MainWidget(BoxLayout):
         self.time_left = 0
         self.one_percent_decrease_threshold = 0
         self.one_percent_decrease = 0
+        self.timer_updated = base_time
         Clock.schedule_interval(self.update, 1.0 / 60)
 
     def on_button_press(self):
         self.hourglass = 0
         self.score += 1
-        self.pos_x = random.randint(0, 100) / 100
-        self.pos_y = random.randint(0, 100) / 100
-        self.time_to_click = self.timer()
+        self.pos_x = random.randint(0, 85) / 100
+        self.pos_y = random.randint(0, 85) / 100
+        self.time_to_click = self.timer(base_time)
         print("pouet")
 
-    def timer(self):
+    def timer(self, base_time):
         # A IMPLEMENTER : TIMER POUR CLIQUER DE PLUS EN PLUS COURT
         self.time_ticking = 0
         self.one_percent_decrease_threshold = 0
         self.one_percent_decrease = 0
         self.loop = 0
-        return 5
+        if self.score == 0:
+            return base_time
+        else:
+            self.timer_updated -= self.timer_updated/100
+            return self.timer_updated
 
     def update(self, dt):
         time_factor = dt * 60
@@ -71,6 +78,7 @@ class MainWidget(BoxLayout):
             self.time_left = 0
             self.one_percent_decrease_threshold = 0
             self.one_percent_decrease = 0
+            self.timer_updated = base_time
             # game over
 
 
